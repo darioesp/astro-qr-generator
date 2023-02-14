af7ec1e20b4462a6080afded0739b6d77b24ec0b
DarioEsp <dario@fixu.cl>
2023-02-14 03:18:44 -0300

    up 🎉: Add deno deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
+import deno from '@astrojs/deno';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-	],
+  output: 'server',
+  adapter: deno(),
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  })]
 });
