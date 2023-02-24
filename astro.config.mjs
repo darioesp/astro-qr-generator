c32d56b0362902c249fa642440b57f5b7250bdac
DarioEsp <dario@fixu.cl>
2023-02-24 12:39:20 -0300

    up: Add preact as astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import deno from '@astrojs/deno';
 import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: deno(),
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  })]
+	output: 'server',
+	adapter: deno(),
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		preact(),
+	],
 });
