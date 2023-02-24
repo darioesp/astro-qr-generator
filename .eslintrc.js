eeabd8ce5bea26e0598773bb64caf747edc61a4c
DarioEsp <dario@fixu.cl>
2023-02-24 12:34:53 -0300

    up: Change eslintr and prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,53 @@
 module.exports = {
 	env: {
 		browser: true,
-		es2021: true,
+		es2022: true,
 		node: true,
 	},
-	extends: ['standard', 'eslint-config-prettier'],
-	overrides: [],
+	extends: [
+		"eslint:recommended",
+		'eslint-config-prettier',
+		"standard",
+		"plugin:astro/recommended",
+	],
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				extraFileExtensions: ['.astro'],
+			},
+			rules: {
+				'astro/no-set-html-directive': 'error',
+			},
+		},
+	],
+	parserOptions: {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
 	},
-	rules: {},
-};
+	rules: {
+		indent: ['warn', 'tab'],
+		'no-unused-vars': 'warn',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_',
+			},
+		],
+		'no-multiple-empty-lines': 'off',
+		'no-tabs': 'off',
+		quotes: ['warn', 'single'],
+		'jsx-quotes': ['warn', 'prefer-single'],
+		'eol-last': 'off',
+	},
+}
